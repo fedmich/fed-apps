ec3bbaf10c70b27e98b601460b85c90e68e78770
Fedmich <fedmich@gmail.com>
2013-10-02 21:36:45 +0800

    RSS - change jobs on categories

--- fb_apps/oilguru/fb.js
+++ fb_apps/oilguru/fb.js
@@ -9,4 +9,19 @@ $(function() {
 	$('#close_gate').click(function(){
 		$('.like_gate').fadeOut('fast');
 	});
+
+	$('#category').change(function(){
+		console.log( $('#category').val() );
+		load_jobs( $('#category').val() )
+	});
+
 });
+
+function load_jobs( cat ){
+	var oTbl = $('#job_listing');
+	oTbl.html('<img src="img/loading.gif" />Loading... ');
+	var url = 'list.php' + ( cat ? '?cat='+cat : '' ) ;
+	$.get( url, '', function (data){
+		oTbl.html( data );
+	} );
+}
