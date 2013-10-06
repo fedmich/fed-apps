46bf59b5b1102d04e5e1ed801cbbe1498f67d3c5
Fedmich <fedmich@gmail.com>
2013-10-07 01:20:48 +0800

    index.php, prevent cache, versioning

--- fb_apps/oilguru/index.php
+++ fb_apps/oilguru/index.php
@@ -1,2 +1,6 @@
 <?php
-	echo file_get_contents( dirname(__FILE__)."/index.htm" );
+	$ver = 3;
+	$content = file_get_contents( dirname(__FILE__)."/index.html");
+	$content = str_replace( '.css"' , '.css?v=' . $ver . '"',$content);
+	$content = str_replace( '.js"' , '.js?v=' . $ver . '"',$content);
+	echo $content;
