8d5ca771079fc32999bcf6d02f1af1d05c4aaccf
Fedmich <fedmich@gmail.com>
2013-10-07 21:18:08 +0800

    Check if FB fan

--- fb_apps/oilguru/index.php
+++ fb_apps/oilguru/index.php
@@ -1,4 +1,26 @@
 <?php
+	if(! empty($_REQUEST["signed_request"])) {
+    	$data = parse_signed_request($_REQUEST["signed_request"]);
+		
+	    if (empty($data["page"]["liked"])) {
+        	echo 'fan';
+    	} else {
+			echo 'not fan';
+    	}
+
+		function parse_signed_request($signed_request) {
+			list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+			$data = json_decode(base64_url_decode($payload), true);
+
+			return $data;
+		}
+
+		function base64_url_decode($input) {
+			return base64_decode(strtr($input, '-_', '+/'));
+		}
+	}
+	
+	
 	$ver = 3;
 	$content = file_get_contents( dirname(__FILE__)."/index.html");
 	$content = str_replace( '.css"' , '.css?v=' . $ver . '"',$content);
