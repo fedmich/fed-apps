4c676973551082f15b62d63480f9165a7a9f327e
Fedmich <fedmich@gmail.com>
2013-10-07 21:38:56 +0800

    show if fb fan

--- fb_apps/oilguru/index.php
+++ fb_apps/oilguru/index.php
@@ -7,17 +7,9 @@
 	if(! empty($_REQUEST["signed_request"])) {
 		var_dump($_REQUEST["signed_request"]);
 
-    	$data = parse_signed_request($_REQUEST["signed_request"]);
-		var_dump($data);
 		
-	    if (empty($data["page"]["liked"])) {
-        	echo 'fan';
-    	} else {
-			echo 'not fan';
-    	}
-
-		function parse_signed_request($signed_request) {
-			list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+	function parse_signed_request($signed_request) {
+			list($encoded_sig, $payload) = explode('.', $signed_request, 2);
 			$data = json_decode(base64_url_decode($payload), true);
 
 			return $data;
@@ -26,6 +18,16 @@
 		function base64_url_decode($input) {
 			return base64_decode(strtr($input, '-_', '+/'));
 		}
+
+		$data = parse_signed_request($_REQUEST["signed_request"]);
+		var_dump($data);
+
+	    if (empty($data["page"]["liked"])) {
+		echo 'fan';
+	} else {
+			echo 'not fan';
+	}
+
 	}
 	
 	
