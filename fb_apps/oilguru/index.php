86e782db9e10a14de96c1266a88a6ef3ec087800
Fedmich <fedmich@gmail.com>
2013-10-07 21:44:58 +0800

    some test code cleanups

--- fb_apps/oilguru/index.php
+++ fb_apps/oilguru/index.php
@@ -1,14 +1,7 @@
 <?php
-	
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-	var_dump($_REQUEST);
-	
 	if(! empty($_REQUEST["signed_request"])) {
-		var_dump($_REQUEST["signed_request"]);
-
 		
-	function parse_signed_request($signed_request) {
+    	function parse_signed_request($signed_request) {
 			list($encoded_sig, $payload) = explode('.', $signed_request, 2);
 			$data = json_decode(base64_url_decode($payload), true);
 
@@ -18,9 +11,9 @@
 		function base64_url_decode($input) {
 			return base64_decode(strtr($input, '-_', '+/'));
 		}
-
+		
 		$data = parse_signed_request($_REQUEST["signed_request"]);
-
+		
 		$is_fan = empty($data["page"]["liked"]) ? false : true;
 
 	}
@@ -30,10 +23,9 @@
 	$content = file_get_contents( dirname(__FILE__)."/index-page.html");
 	$content = str_replace( '.css"' , '.css?v=' . $ver . '"',$content);
 	$content = str_replace( '.js"' , '.js?v=' . $ver . '"',$content);
-	echo 'php';
-
+	
 	if(! empty($is_fan)){
 		$content = str_replace( 'var is_fan = false', 'var is_fan = true',$content);
 	}
-
+	
 	echo $content;
